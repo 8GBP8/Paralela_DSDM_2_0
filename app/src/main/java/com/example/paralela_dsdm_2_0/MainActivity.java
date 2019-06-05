21ecf122723d51c5b61df14c39e78132ff2a02f6
Gustavo <gutobp01020304@gmail.com>
2019-06-05 12:36:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/paralela_dsdm_2_0/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.paralela_dsdm_2_0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
